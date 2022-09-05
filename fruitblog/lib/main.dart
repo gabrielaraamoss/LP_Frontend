946fab878a9928fb0118bf820fa9666acd8f4c20
gabrielaraamoss <gabrielaramosbaq@icloud.com>
2022-09-05 11:28:19 -0500

    Rebase with main

--- fruitblog/lib/main.dart
+++ fruitblog/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruitblog/pages/home_page/home_page.dart';
+import 'package:fruitblog/components/side_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,96 +29,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(
       body: Row(children: <Widget>[
         SideLayout(),
         Expanded(flex: 3, child: Stack(children: [HomePage()]))
       ]),
     );
-  }
-}
-
-class SideLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: const Color.fromARGB(255, 0, 0, 0),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 70),
-            RichText(
-              text: const TextSpan(
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w800),
-                children: <TextSpan>[
-                  TextSpan(text: 'Fr'),
-                  TextSpan(
-                      text: 'es',
-                      style: TextStyle(color: Color.fromARGB(255, 255, 0, 0))),
-                  TextSpan(
-                      text: 'h F',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 251, 235, 0))),
-                  TextSpan(
-                      text: 'ru',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 251, 167, 0))),
-                  TextSpan(
-                      text: 'its',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 50, 202, 90))),
-                ],
-              ),
-            ),
-            Image.asset(
-              'assets/fruit.png',
-              height: 70,
-              width: 70,
-            ),
-            const SizedBox(height: 30),
-            Expanded(
-                child: Column(
-              children: const <Widget>[
-                Text(
-                  'Inicio',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                      color: Color.fromARGB(223, 97, 97, 93),
-                      decoration: TextDecoration.none,
-                      fontSize: 30),
-                ),
-                SizedBox(height: 50),
-                Text('Blog',
-                    style: TextStyle(
-                        color: Color.fromARGB(223, 97, 97, 93),
-                        decoration: TextDecoration.underline,
-                        decorationColor: Color.fromARGB(223, 97, 97, 93),
-                        fontSize: 30)),
-                SizedBox(height: 50),
-                Text('Producto',
-                    style: TextStyle(
-                        color: Color.fromARGB(223, 97, 97, 93),
-                        decoration: TextDecoration.none,
-                        fontSize: 30)),
-                SizedBox(height: 50),
-                Text('Contacto',
-                    style: TextStyle(
-                        color: Color.fromARGB(223, 97, 97, 93),
-                        decoration: TextDecoration.none,
-                        fontSize: 30)),
-              ],
-            ))
-          ],
-        ),
-      ),
-    );
-=======
-    return Row(
-        children: <Widget>[SideLayout(), Expanded(flex: 3, child: Text(''))]);
->>>>>>> e6b5d8f (Reorganizando)
   }
 }
